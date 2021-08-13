6737544566495e21be95878c0b52155382e028b7
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-08-13 18:03:25 +0100

    Create testkillbrick.lua

---
+++ testkillbrick.lua
@@ -0,0 +1,8 @@
+local function trapWasTouched (otherPart)
+	local humanoid=otherPart.parent:FindFirstChildWhichInA("Humanoid")
+	if humanoid then
+		humanoid.Health=0
+		end
+	end
+end
+script.Parent.Touched:Connect(trapWasTouched)
