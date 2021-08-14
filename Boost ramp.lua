832ed1450bc6d52b9e93aea17a41cfe72952ce48
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-08-14 10:46:24 +0100

    Update Boost ramp.lua

--- Boost ramp.lua
+++ Boost ramp.lua
@@ -1,7 +1,11 @@
 local function boostWasTouched(otherPart)
-  local humanoid=otherPart.Parent:FirstChildWhichInA("Humanoid")
-  if humanoid then
-    
-  end
+	local humanoid=otherPart.Parent:FindFirstChildWhichIsA("Humanoid")
+	if humanoid then
+		humanoid.WalkSpeed=46
+		humanoid.JumpHeight=100
+		wait(6)
+		humanoid.WalkSpeed=16
+		humanoid.JumpHeight=7.2
+	end
 end
 script.Parent.Touched:Connect(boostWasTouched)
