cabd528b6cdd1d97b6aa03f401b8a3b81a5de76f
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-09-05 10:13:48 +0100

    Update push.lua

--- push.lua
+++ push.lua
@@ -1,11 +1,9 @@
---code used for a conveyor belt, you need to add an attribute for this
+--code used for a conveyor belt, you need to add an attribute for this and two attachments "attstart and attend"
 --saved to github to be pasted into studio as a block to push you one way
-
 --variables
 local conveyor = script.Parent
 local attStart = conveyor:FindFirstChild("AttStart")
 local attEnd = conveyor:FindFirstChild("AttEnd")
-
 --make function
 local function setVelocity()
 	local direction = attEnd.WorldPosition - attStart.WorldPosition
