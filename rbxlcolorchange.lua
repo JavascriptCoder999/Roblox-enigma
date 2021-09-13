710d7e240246a4b812339f9349345b2311628a9e
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-09-13 16:51:13 +0100

    Create rbxlcolorchange.lua

---
+++ rbxlcolorchange.lua
@@ -0,0 +1,43 @@
+wait(8)
+local colors={}
+local tiles=script.Parent.Tiles:GetChildren();
+for i=1,#tiles do
+	local tile=tiles[i]
+	local tileColor=tile.BrickColor;
+	table.insert(colors,i,tileColor);
+		
+end
+function pickColor()
+	local randNum=math.random(1,#colors);
+	local color=colors[randNum];
+	return color;
+end
+local timer=5;
+local timerChange=.1;
+function hideTiles(color)
+	for i=1,#tiles do
+		if(tiles[i].BrickColor~=color)then
+			tiles[i].Transparency=1;
+			tiles[i].CanCollide=false;
+			
+		end
+	end
+end
+function showTiles()
+	for i=1,#tiles do
+		tiles[i].Transparency=0;
+		tiles[i].canCollide=true;
+	end
+end
+function runGame()
+	local color=pickColor;
+	script.Parent.Display.BrickColor=color;
+	wait(timer);
+	hideTiles(color)
+	wait(1)
+	showTiles()
+	timer=timer-timerChange;
+end
+while(timer>1)do
+	runGame();
+end
