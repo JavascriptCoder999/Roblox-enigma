967ebff1dc84635dd72dd102a2b4fcdbceb9003c
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-12-18 14:12:21 +0000

    Delete testkillbrick.lua

--- testkillbrick.lua
+++
@@ -1,7 +0,0 @@
-local function trapWasTouched (otherPart)
-	local humanoid=otherPart.Parent:FindFirstChild("Humanoid")
-	if humanoid then
-		humanoid.Health=0
-		end
-	end
-script.Parent.Touched:Connect(trapWasTouched)
