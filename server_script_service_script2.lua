32a339d804caaa7c2fe51040134beb32866c78d4
FuturamaGuy2017 <lucaspeel93@outlook.com>
2022-01-09 13:04:06 +0000

    Update server_script_service_script2.lua

--- server_script_service_script2.lua
+++ server_script_service_script2.lua
@@ -2,7 +2,8 @@ local PositionFolder=game.Workspace.Pos:GetChildren()
 while true do
 	local randomPosition=PositionFolder[math.random(1, #PositionFolder)]
 	local lightningClone=game.ServerStorage.lightningUnion:Clone()
-	lightningClone.Positon=randomPosition
+	lightningClone.Parent = workspace
+	lightningClone.Position=randomPosition.position
 	wait(0.5)
 	lightningClone:Destroy()
 	wait(0.5)
