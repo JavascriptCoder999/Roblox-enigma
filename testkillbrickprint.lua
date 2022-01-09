243638c9a2772fce58a1b07e9c419fd355324bda
FuturamaGuy2017 <lucaspeel93@outlook.com>
2022-01-09 12:28:12 +0000

    Delete testkillbrickprint.lua

--- testkillbrickprint.lua
+++
@@ -1,5 +0,0 @@
-local function trapWasTouched (otherPart)
-	local humanoid=otherPart.parent:FindFirstChildWhichInA("Humanoid")
-	print("Trap Activated!")
-end
-script.Parent.Touched:Connect(trapWasTouched)
