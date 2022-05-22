575d59ddbbd789fc5c122f4888bb918c1b22f9a5
FuturamaGuy2017 <lucaspeel93@outlook.com>
2022-05-22 10:13:50 +0100

    Update doorcode.lua

--- doorcode.lua
+++ doorcode.lua
@@ -1,22 +1,23 @@
 local door=script.Parent
 local MarketplaceService=game:GetService("MarketplaceService")
-local Players=game:GetService("Players")
-local gamePassID=35698359
+local players=game:GetService("Players")
+local gamePassID=46081985
 local function doorTouch(part)
-	local player=Players:GetPlayerFromCharacter(part.Parent)
+	local player=players:GetPlayerFromCharacter(part.Parent)
 	local hasPass=false
 	local success, message=pcall(function()
 		hasPass=MarketplaceService:UserOwnsGamePassAsync(player.UserId, gamePassID)
 	end)
-end
-if not success then
-	warn("Error while checking if player has pass: "..tostring(message))
-	return
-		if hasPass==true then
+	if not success then
+		warn("Error while checking if player has pass: "..tostring(message))
+		return
+	end
+	if hasPass==true then
 		door.CanCollide=false
+	end
 end
-	door.Touched:Connect(doorTouch)
-	while true do
-	door.CanCollide=true		
+door.Touched:Connect(doorTouch)
+while true do
+	door.CanCollide=true
 	wait(0.5)
 end
